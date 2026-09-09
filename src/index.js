52758aca474aa63b8e53f7b268cc78f534c34a82
junaidahmed-One <junaidahmed882000@gmail.com>
2026-09-09 13:50:04 +0530

    add router to different backend services and add zod validation schema

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { matchRouter } from "./routes/matches";
 
 const app = express();
 
@@ -7,6 +8,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello from Express server");
 });
+
+app.use("/matches", matchRouter);
 
 app.listen(8000, () => {
   console.log(`Server is running at http://localhost:8000`);
